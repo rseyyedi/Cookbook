2130219ce56c89f829138865627d60f39cb28cd9
Razi Seyyedi <rseyyedi@users.noreply.github.com>
2018-05-31 11:42:02 +0200

    Add master receive

--- c/I2C.c
+++ c/I2C.c
@@ -9,11 +9,13 @@ typedef u8 AddressType;
 unsigned I2CMaster(u16 DeviceId, u8 *SendBuffer, u8 *ReadBuffer, u16 ByteCount, AddressType SlaveAddr)
 {
 	volatile unsigned SentByteCount;
+	volatile unsigned ReceivedByteCount;
 	do {
 		SentByteCount = XIic_Send(IIC_BASE_ADDRESS, MOTOR1_ADDRESS,
 						  SendBuffer, ByteCount,
 						  XIIC_STOP);
 		if (SentByteCount != ByteCount) {
+
 			/* Send is aborted so reset Tx FIFO */
 			XIic_WriteReg(IIC_BASE_ADDRESS,  XIIC_CR_REG_OFFSET,
 					XIIC_CR_TX_FIFO_RESET_MASK);
@@ -23,5 +25,9 @@ unsigned I2CMaster(u16 DeviceId, u8 *SendBuffer, u8 *ReadBuffer, u16 ByteCount, 
 
 	} while (SentByteCount != ByteCount);
 
-	return SentByteCount - ByteCount;
+	ReceivedByteCount = XIic_Recv(IIC_BASE_ADDRESS, MOTOR1_ADDRESS,
+				ReadBuffer,
+				ByteCount, XIIC_STOP);
+
+	return (SentByteCount - ByteCount) || (ReceivedByteCount - ByteCount);
 }
